662e989c04d2d81afd88abf20f88db3638504f9f
Lucas Benevides <professorbenevides@gmail.com>
2026-06-30 21:52:19 -0300

    Gera erro para fins didáticos

--- sistema.py
+++ sistema.py
@@ -5,7 +5,7 @@ def somar_valores(a, b):
 # Validação do Sistema (Teste Automatizado)
 def test_somar_valores():
     # Espera-se que 2 + 3 seja estritamente igual a 5
-    assert somar_valores(2, 3) == 5
+    assert somar_valores(2, 3) == 6
     print("Sucesso: A validação matemática do sistema passou em conformidade!")
 
 if __name__ == "__main__":
